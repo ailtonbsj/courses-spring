669db04d10a6869e9e2b9581c69e7511156d29dc
ailtonbsj <ailton.ifce@gmail.com>
2022-08-23 00:55:36 -0300

    started spring project and modelling domain

---
+++ jwt-spring-and-angular/src/main/java/br/ailtonbsj/usermanagerdemo/UsermanagerDemoApplication.java
@@ -0,0 +1,13 @@
+package br.ailtonbsj.usermanagerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagerDemoApplication.class, args);
+	}
+
+}
---
+++ jwt-spring-and-angular/src/test/java/br/ailtonbsj/usermanagerdemo/UsermanagerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ailtonbsj.usermanagerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
