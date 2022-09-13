c734c3db0a5732d409b3d5ae6b9b1a1e8832cc15
ailtonbsj <ailton.ifce@gmail.com>
2022-09-12 23:35:09 -0300

    Removed deprecated configuration in SecurityConfig

--- jwt-spring-security-getarrays/src/main/java/br/ailtonbsj/jwtspringsecurity/api/UserController.java
+++ jwt-spring-security-getarrays/src/main/java/br/ailtonbsj/jwtspringsecurity/api/UserController.java
@@ -16,8 +16,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -33,8 +34,6 @@ import br.ailtonbsj.jwtspringsecurity.domain.UserModel;
 import br.ailtonbsj.jwtspringsecurity.dto.RoleToUserFormDTO;
 import br.ailtonbsj.jwtspringsecurity.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api")
--- jwt-spring-security-getarrays/src/main/java/br/ailtonbsj/jwtspringsecurity/security/SecurityConfig.java
+++ jwt-spring-security-getarrays/src/main/java/br/ailtonbsj/jwtspringsecurity/security/SecurityConfig.java
@@ -1,17 +1,16 @@
 package br.ailtonbsj.jwtspringsecurity.security;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import static org.springframework.http.HttpMethod.*;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import br.ailtonbsj.jwtspringsecurity.filter.CustomAuthenticationFilter;
@@ -22,37 +21,28 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final UserDetailsService userDetailsService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(
-                authenticationManagerBean());
-        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().antMatchers("/api/login/**", "/token/refresh/**").permitAll();
-        http.authorizeRequests().antMatchers(GET, "/api/users/**")
-                .hasAnyAuthority("ROLE_SUPER_ADMIN", "ROLE_ADMIN");
-        http.authorizeRequests().antMatchers(POST, "/api/user/save/**")
-                .hasAnyAuthority("ROLE_ADMIN");
-        // http.authorizeRequests().antMatchers("/api/greeting").hasAnyAuthority("ROLE_SUPER_ADMIN");
-        http.authorizeRequests().anyRequest().authenticated();
-        http.addFilter(customAuthenticationFilter);
-        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+public class SecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(
+                authenticationConfiguration.getAuthenticationManager());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+
+        http.csrf().disable().cors()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests().antMatchers("/api/login/**", "/token/refresh/**").permitAll()
+                .antMatchers(GET, "/api/users/**").hasAnyAuthority("ROLE_SUPER_ADMIN", "ROLE_ADMIN")
+                .antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN")
+                // .antMatchers("/api/greeting").hasAnyAuthority("ROLE_SUPER_ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(customAuthenticationFilter)
+                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
 }
